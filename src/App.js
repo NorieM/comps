7855b842ccb0bd394821af7cb44ce40d49c7caf6
Norie MacEwan <50264139+NorieM@users.noreply.github.com>
2023-01-28 10:39:06 +0000

    Added Tailwind

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import Button from "./Button";
+
 function App() {
-  return <div>Comps</div>
+  return (<div>
+    <div>
+      <Button primary secondary>Primary</Button>
+    </div>
+    <div>
+      <Button>Secondary</Button>
+    </div>
+    <div>
+      <Button>Success</Button>
+    </div>
+    <div>
+      <Button>Warning</Button>
+    </div>
+    <div>
+      <Button>Danger</Button>
+    </div>
+  </div>)
 }
 
  export default App;
