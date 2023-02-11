8df8968fb61824587a9a075060a49ad3fa04a326
Norie MacEwan <50264139+NorieM@users.noreply.github.com>
2023-02-11 12:26:48 +0000

    Styled active link in sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,18 +5,19 @@ function Sidebar() {
     { label: "Dropdown", path: "/" },
     { label: "Accordion", path: "/accordion" },
     { label: "Buttons", path: "/buttons" },
+    { label:"Modal", path: "/modal"}
   ];
 
   const renderedLinks = links.map((link) => {
     return (
-      <Link to={link.path} key={link.label}>
+      <Link to={link.path} key={link.label} className='mb-3' activeClassName='font-bold border-l-4 border-blue-500 pl-2'>
         {link.label}
       </Link>
     );
   });
 
   return (
-    <div className="sticky top-0 overflow-y-scroll flex flex-col">
+    <div className="sticky top-0 overflow-y-auto flex flex-col items-start">
         {renderedLinks}
     </div>);
 }
