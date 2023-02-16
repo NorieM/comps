ca840d6bda4f0ab62509fcab810a55cfc0d873b8
Norie MacEwan <50264139+NorieM@users.noreply.github.com>
2023-02-16 22:36:24 +0000

    Added SortableTable component

--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,4 +1,5 @@
-import Table from "../components/Table";
+// import Table from "../components/Table";
+import SortableTable from "../components/SortableTable";
 
 function TablePage() {
 
@@ -12,7 +13,8 @@ function TablePage() {
   const config = [
     {
       label: 'Name',
-      render: (fruit) => fruit.name
+      render: (fruit) => fruit.name,
+      sortValue: (fruit) => fruit.name
     },
     {
       label: 'Color',
@@ -20,7 +22,8 @@ function TablePage() {
     },
     {
       label: 'Score',
-      render: (fruit) => fruit.score
+      render: (fruit) => fruit.score,
+      sortValue: (fruit) => fruit.score
     }
   ];
 
@@ -29,7 +32,7 @@ function TablePage() {
   }
   return (
     <div>
-      <Table data={data} config={config} keyFn={keyFn}/>
+      <SortableTable data={data} config={config} keyFn={keyFn}/>
     </div>
   )
 }
