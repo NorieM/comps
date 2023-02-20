aa16c501e6acd0ae08752570a662d95923dbdca4
Norie MacEwan <50264139+NorieM@users.noreply.github.com>
2023-02-20 19:28:29 +0000

    Added custom sort hook

---
+++ src/pages/CounterPage.js
@@ -0,0 +1,23 @@
+import useCounter from '../hooks/use-counter';
+import Button from "../components/Button";
+
+
+
+function CounterPage({ initialCount }) {
+
+  const { count, increment } = useCounter(initialCount);
+
+  return (
+    <div>
+      <h1>Count is {count}</h1>
+      <Button onClick={increment} primary rounded>
+        Increment
+      </Button>
+    </div>
+
+  );
+
+}
+
+export default CounterPage;
+
